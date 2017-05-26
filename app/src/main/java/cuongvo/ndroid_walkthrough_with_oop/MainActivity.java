c208b43aac4f2e7bcc652130ed5209e32617a1bc
Cuong Vo <baongoc@Cuong-Vo-Macbook.local>
2017-05-26 17:05:14 +0700

    Init project

---
+++ app/src/main/java/cuongvo/ndroid_walkthrough_with_oop/MainActivity.java
@@ -0,0 +1,13 @@
+package cuongvo.ndroid_walkthrough_with_oop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/cuongvo/ndroid_walkthrough_with_oop/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package cuongvo.ndroid_walkthrough_with_oop;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
